2292b1111b95aa04f2ebef5a184c75326b1a33d2
Санёк Виничук <sashavinichuk@gmail.com>
2022-08-05 09:16:29 +0300

    Авто Ресурс Сервис 1.0

--- Авто Ресурс Сервис/Windows/NewsAddWindow.xaml.cs
+++ Авто Ресурс Сервис/Windows/NewsAddWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +14,8 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using Авто_Ресурс_Сервис.Http;
+using Авто_Ресурс_Сервис.Parser;
 
 namespace Авто_Ресурс_Сервис.Windows
 {
@@ -24,9 +29,43 @@ namespace Авто_Ресурс_Сервис.Windows
             InitializeComponent();
         }
 
+        private string fileType = "";
+        private byte[] fileName;
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            News news = new News();
+            news.NameNews = NameNews.Text;
+            news.BaseInfo = BaseNews.Text;
+            news.AllInfo = AllNews.Text;
+            news.DateTime = DateTime.Now;
+            news.Image = fileName;
+            news.ImageMimeTypeOfData = fileType;
+            news.NewsLinkSrc = LinkOnNews.Text;
+            var data = JsonConvert.SerializeObject(news);
+            Post.Send("News", "AddNews", data);
+            this.Close();
+        }
 
+        private void AddImage_Click(object sender, RoutedEventArgs e)
+        {
+            string fileContent;
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.ShowDialog();
+            var fileStream = fileDialog.OpenFile();
+
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                fileContent = reader.ReadToEnd();
+                byte[] arr = Encoding.Unicode.GetBytes(fileContent);
+                MemoryStream ms = new MemoryStream(arr);
+                fileName = ms.ToArray();
+            }
+
+            fileType = Path.GetExtension(fileDialog.FileName);
+            //byte[] bData = File.ReadAllBytes(fileDialog.FileName);
+            //fileName = bData;
+            Trace.WriteLine(fileDialog);
+            
         }
     }
 }
